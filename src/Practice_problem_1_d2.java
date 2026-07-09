f619aed4aabd1fda4b59208f34e3b2493a9a412a
ADIL102214 <2024831066@student.sust.edu>
2026-07-09 10:24:02 +0600

    CHECK OF ODD OR EVEN NUMBER

--- src/Practice_problem_1_d2.java
+++ src/Practice_problem_1_d2.java
@@ -2,12 +2,19 @@ import java.util.Scanner;
 public class Practice_problem_1_d2 {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
+        //CHECK OF POSITIVE INTEGER
         System.out.print("Input : ");
         int num = scanner.nextInt();
         if(num > 0){
             System.out.println("Positive integer");
         }else{
             System.out.println("Not a positive integer");
+        }
+        //CHECK OF ODD OR EVEN NUMBER
+        if(num%2==0){
+            System.out.println("Even number");
+        }else{
+            System.out.println("Odd number");
         }
     }
 }
