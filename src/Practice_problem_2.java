bdc408fa66f90bad0861b02e67b854ea2daea7e8
ADIL102214 <2024831066@student.sust.edu>
2026-07-13 01:19:40 +0600

    SERIES PRINTING WITH EVERY LOOP

--- src/Practice_problem_2.java
+++ src/Practice_problem_2.java
@@ -8,5 +8,31 @@ public class Practice_problem_2{
         }else{
             System.out.printf("%d is not a leap year\n", year);
         }
+        for (int i = 2; i<=20; i+=2){
+            System.out.print(i + (i < 20 ? ", " : " \n"));
+        }
+        for (int i = 1; i<20; i+=2){
+            System.out.print(i + (i < 19 ? ", " : " \n"));
+        }
+        int i = 2;
+        while (i <= 20){
+            System.out.print(i + (i < 20 ? ", " : " \n"));
+            i+=2;
+        }
+        i = 1;
+        while (i < 20){
+            System.out.print(i + (i < 19 ? ", " : " \n"));
+            i+=2;
+        }
+        i = 2;
+        do {
+            System.out.print(i + (i < 20 ? ", " : " \n"));
+            i += 2;
+        } while (i <= 20);
+        i = 1;
+        do {
+            System.out.print(i + (i < 19 ? ", " : " \n"));
+            i += 2;
+        } while (i < 20);
     }
 }
